8d6a4b1ceda287341990635bb2b009b5ef914db6
yusuf20090 <103944934+yusuf20090@users.noreply.github.com>
2022-08-08 20:16:09 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let hamburger = document.querySelector('.hamburger');
+let NavCurtuan = document.querySelector('.nav__curtuan');
 hamburger.onclick = function(){
-    hamburger.classList.toggle('hamburger_active')
+    hamburger.classList.toggle('hamburger_active');
+    NavCurtuan.classList.toggle('nav__curtuan_show');
 }
