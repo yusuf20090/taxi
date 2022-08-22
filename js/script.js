43714edb76d021f6b9a22a41d1af0d7927f29a67
yusuf20090 <103944934+yusuf20090@users.noreply.github.com>
2022-08-22 17:32:21 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 let hamburger = document.querySelector('.hamburger');
 let NavCurtuan = document.querySelector('.nav__curtuan');
-hamburger.onclick = function(){
+hamburger.onclick = function () {
     hamburger.classList.toggle('hamburger_active');
     NavCurtuan.classList.toggle('nav__curtuan_show');
 }
+
+const swiper = new Swiper('.ways__list', {
+    slidesPerView: 2.5,
+    spaceBetween: 20,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 2.5,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3.5,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 4.5,
+            spaceBetween: 40
+        },
+        768: {
+            slidesPerView: 5.5,
+            spaceBetween: 40
+        },
+        900: {
+            slidesPerView: 6.5,
+            spaceBetween: 40
+        }
+    }
+});
