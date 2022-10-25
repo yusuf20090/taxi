547a50225708d2c2ad923eea56cf7e09fedf21ba
yusuf20090 <103944934+yusuf20090@users.noreply.github.com>
2022-10-25 20:33:16 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,30 +7,25 @@ hamburger.onclick = function () {
 
 const swiper = new Swiper('.ways__list', {
     slidesPerView: 2.5,
-    spaceBetween: 20,
+    spaceBetween: 16,
     breakpoints: {
         // when window width is >= 320px
         320: {
             slidesPerView: 2.5,
-            spaceBetween: 20
         },
         // when window width is >= 480px
         480: {
             slidesPerView: 3.5,
-            spaceBetween: 30
         },
         // when window width is >= 640px
         640: {
             slidesPerView: 4.5,
-            spaceBetween: 40
         },
         768: {
             slidesPerView: 5.5,
-            spaceBetween: 40
         },
         900: {
             slidesPerView: 6.5,
-            spaceBetween: 40
         }
         
     }
